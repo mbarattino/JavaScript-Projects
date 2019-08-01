9380ecba336cdb2864af620c09beeb649265827a
Matt <mbarattino25@gmail.com>
2019-08-01 12:48:22 -0500

    initial commit

---
+++ JS/JavaScript_1.js
@@ -0,0 +1,50 @@
+alert("Hello World!");
+
+window.alert("Hello World!");
+
+document.write('Hello World!');
+
+document.write("<br>");
+
+var A = "This is a string";
+document.write(A);
+
+document.write("<br>");
+
+var Texas = "Don't mess with Texas!"
+window.alert(Texas);
+
+document.write("\"Adding in quotations\"");
+
+document.write("<br>");
+
+document.write("Make sure" + " you brush your teeth" + " before bed");
+
+document.write("<br>");
+
+var T = "Texas" + " Rangers"
+document.write(T);
+
+document.write("<br>");
+
+var Mom = "Lauriana", Dad = "Matt", Child = "Dallas";
+document.write(Child);
+
+document.write("<br>");
+
+var Mom = Mom.fontcolor("Green");
+var Dad = Dad.fontcolor("Blue");
+var Child = Child.fontcolor("Red");
+document.write(Child);
+
+document.write("<br>");
+
+document.write(4+4);
+
+document.write("<br>");
+
+function My_First_Function() {
+    var String = "This text is green!";
+    var result = String.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
