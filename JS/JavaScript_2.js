468c6c8162068779768f2032509785cf081b06f5
Matt <mbarattino25@gmail.com>
2019-08-01 13:46:41 -0500

    concatenate a string

---
+++ JS/JavaScript_2.js
@@ -0,0 +1,11 @@
+function my_Second_Function() {
+    var String = "This text is blue";
+    var result = String.fontcolor("blue");
+    document.getElementById("Blue_Text").innerHTML = result;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
